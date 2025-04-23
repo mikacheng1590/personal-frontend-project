6073202fd9be4e2f1bc8522f6cba55c065080492
Mika Fung <fungcheukman@hotmail.com>
2025-04-23 12:23:32 -0700

    remove 100vh from main and set the height for each section instead

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,7 +2,6 @@
 
 import { Name, SelfIntroTitle, HorizontalSection, SelfIntro, Carousel, Video } from "./_client-components";
 
-const mainContainer = "main-container"
 const selfIntroSection = "self-intro-section"
 const professionalExperienceSection = "professional-experience-section"
 const selfProjectsSection = "self-projects-section"
@@ -11,11 +10,11 @@ const personalVideoSection = "personal-video-section"
 export default function Home() {
 
   return (
-    <main className={`${mainContainer} h-screen w-full`}>
-      <section className="my-name-section h-full min-h-[400px] w-full bg-mika-yellow-50 flex items-center justify-center overflow-hidden">
+    <main>
+      <section className="my-name-section h-full h-screen min-h-[400px] w-full bg-mika-yellow-50 flex items-center justify-center overflow-hidden">
         <Name />
       </section>
-      <section className={`${selfIntroSection} h-full min-h-[900px] w-full bg-mika-yellow-100 flex items-center justify-center overflow-hidden`}>
+      <section className={`${selfIntroSection} h-screen min-h-[900px] w-full bg-mika-yellow-100 flex items-center justify-center overflow-hidden`}>
         <div className="flex flex-col items-center justify-center h-full container p-4 md:p-8">
           <div className="mb-4 md:mb-8">
             <SelfIntroTitle 
@@ -30,7 +29,7 @@ export default function Home() {
       <section className={`${professionalExperienceSection} overflow-x-hidden`}>
         <HorizontalSection />
       </section>
-      <section className={`${selfProjectsSection} h-full min-h-[800px] w-full bg-mika-blue-100 flex items-center justify-center`}>
+      <section className={`${selfProjectsSection} h-screen min-h-[800px] w-full bg-mika-blue-100 flex items-center justify-center`}>
         <div className="container">
           <h6 className="text-mika-blue-50 text-2xl md:text-6xl font-bold uppercase text-center mb-4 md:mb-8">
             Something I've Built
@@ -38,7 +37,7 @@ export default function Home() {
           <Carousel />
         </div>
       </section>
-      <section className={`${personalVideoSection} h-full min-h-[800px] w-full bg-mika-green-50 flex items-center justify-center`}>
+      <section className={`${personalVideoSection} h-screen min-h-[800px] w-full bg-mika-green-50 flex items-center justify-center`}>
         <div className="container flex items-center justify-center p-4 md:p-8">
           <Video
             src="https://txrfmzxgqvckqzumiquw.supabase.co/storage/v1/object/public/misc/showcase-hb100.mp4"
