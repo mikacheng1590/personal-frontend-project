b437a7589b3b38a44d79df057eb62ff21aae2ce9
Mika Fung <fungcheukman@hotmail.com>
2025-04-30 17:41:28 -0700

    further increase contact icon size in mobile view

--- src/app/(home)/_client-components/contact-me/ContactIconRow.tsx
+++ src/app/(home)/_client-components/contact-me/ContactIconRow.tsx
@@ -3,15 +3,15 @@ import { FaEnvelope, FaLinkedin, FaGithub } from "react-icons/fa";
 
 const contacts = [
   {
-    icon: <FaEnvelope className="text-3xl md:text-6xl"/>,
+    icon: <FaEnvelope className="text-5xl md:text-6xl"/>,
     link: "mailto:fungcheukman@hotmail.com"
   },
   {
-    icon: <FaLinkedin className="text-3xl md:text-6xl"/>,
+    icon: <FaLinkedin className="text-5xl md:text-6xl"/>,
     link: "https://linkedin.com/in/mika-fung"
   },
   {
-    icon: <FaGithub className="text-3xl md:text-6xl"/>,
+    icon: <FaGithub className="text-5xl md:text-6xl"/>,
     link: "https://github.com/mikacheng1590"
   }
 ]
